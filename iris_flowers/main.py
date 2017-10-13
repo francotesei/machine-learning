e349370dd3e2bc27741630a8fee08eae6bdbc5a9
francotesei <ftesei96@gmail.com>
2017-10-13 19:05:59 -0300

    added iris_flowers

---
+++ iris_flowers/main.py
@@ -0,0 +1,22 @@
+# https://www.youtube.com/watch?v=tNa99PG8hR8
+
+from sklearn.datasets import load_iris
+from sklearn import tree
+import numpy as np
+
+iris = load_iris();
+test_idx = [1,50,100]
+
+#training_data
+train_target = np.delete(iris.target,test_idx)
+train_data = np.delete(iris.data,test_idx,axis = 0)
+
+#testing_data
+test_target = iris.target[test_idx]
+test_data = iris.data[test_idx]
+
+clf = tree.DecisionTreeClassifier()
+clf.fit(train_data,train_target)
+
+print (test_target)
+print(clf.predict(test_data))
